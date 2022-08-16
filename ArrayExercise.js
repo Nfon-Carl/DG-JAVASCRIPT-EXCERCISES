6fbe527688812d51391f862fc9d6f99f02189043
Nfon-Carl <nfoncarl48@gmail.com>
2022-08-16 11:11:08 +0100

    Array Excercise 1 to 6 done

--- ArrayExercise.js
+++ ArrayExercise.js
@@ -72,3 +72,22 @@ function join(input,seperator){
 
 
 //6 program which accept a number as input and insert dashes (-) between each two even numbers.
+
+function dashInsert(num){
+    
+const str = num.toString();
+const result = [str[0]];
+  
+for(let x=1; x<str.length; x++)
+  {
+    if((str[x-1]%2 === 0)&&(str[x]%2 === 0))
+     {
+      result.push('-', str[x]);
+     }
+    else
+     {
+      result.push(str[x]);
+     }
+  }
+console.log(result.join(''));
+}
--- DomExcercises.js
+++ DomExcercises.js
@@ -1,3 +1,53 @@
-function js_style(){
-    
+let styleButton = document.querySelector("jsstyle");
+// let submitButton = document.querySelector("submitbtn")
+
+function changeState(){
+
+    if(state===1)
+    {
+        state=0
+        js_style(state)
+    }
+     if (state===0){
+        state=1
+        js_style(state)
+    }
 }
+
+function js_style(state) 
+{
+//font styles added by JS:
+
+if (state === 1){
+
+
+ text.style.fontSize = "14pt";
+ text.style.fontFamily = "Comic Sans MS";
+ text.style.color = "green";
+
+}
+if (state===0){
+    text.style.fontSize = "10pt";
+    text.style.fontFamily = "Tahoma";
+    text.style.color = "black";
+}
+
+}
+
+
+function getFormvalue()
+{
+  let x=document.getElementById("form1");
+  for (let i=0;i<x.length;i++)
+  {
+   if (x.elements[i].value!='Submit')
+    {  
+      console.log(x.elements[i].value);
+     }  
+   }
+}
+
+
+
+styleButton.addEventListener("click", changeState);
+
